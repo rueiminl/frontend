64883b87d026f8746833a5153ed2c11749868394
Ruei-Min Lin <rueiminl@unix3.andrew.cmu.edu>
2014-10-14 01:20:09 -0400

    complete q1

---
+++ q1.php
@@ -0,0 +1,8 @@
+<?php
+	$X = "6876766832351765396496377534476050002970857483815262918450355869850085167053394672634315391224052153";
+	$XY = $_GET["key"];
+	include "util.php";
+	echo bcdiv($XY, $X)."<br>";
+	echo $team_id.",".$aws_account_id[0].",".$aws_account_id[1].",".$aws_account_id[2]."<br>";
+	echo date('Y-m-d H:i:s');
+?>
