ab17158b62ea7afb612896f538f01f8385aa716d
Ray0617 <lrm0617@gmail.com>
2014-10-21 21:29:36 -0400

    fix q2 mysql utf8

--- q2.php
+++ q2.php
@@ -4,14 +4,12 @@
 	$username = "root";
 	$dbname = "test";
 
-	// Create connection
 	$conn = new mysqli($servername, $username, $password, $dbname);
-
-	// Check connection
 	if ($conn->connect_error) 
 	{
 		die("Database connection failed: " . $conn->connect_error);
 	}
+	$conn->set_charset("utf8");
 	include "util.php";
 	echo $team_id.",".$aws_account_id[0].",".$aws_account_id[1].",".$aws_account_id[2]."\n";
 	$query = "select tid, sentiment, text from tweet where uid=".$_GET["userid"]." and ts=\"".str_replace("+", " ", $_GET["tweet_time"])."\" order by tid";
