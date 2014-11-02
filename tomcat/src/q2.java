6b0cbf82802b546df9385d0da9be7f4ffd51f701
Ruei-Min Lin <rueiminl@andrew.cmu.edu>
2014-11-02 05:03:37 +0000

    fix utf8 in q2

--- tomcat/src/q2.java
+++ tomcat/src/q2.java
@@ -26,7 +26,7 @@ public class q2 extends HttpServlet {
 	try 
 	{
 		Class.forName("com.mysql.jdbc.Driver");
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db15619?user=root&password=wolken");
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db15619?user=root&password=wolken&characterEncoding=UTF-8");
 	}
 	catch (Exception e)
 	{
@@ -36,13 +36,15 @@ public class q2 extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // Set response content type
-        response.setContentType("text/plain");
+        response.setContentType("text/plain; charset=UTF-8");
+	response.setCharacterEncoding("UTF-8");
         // Actual logic goes here.
         PrintWriter out = response.getWriter();
         out.println("Wolken,5534-0848-5100,0299-6830-9164,4569-9487-7416");
 	try 
 	{
 		String query = "select concat(tid, \":\", s, \":\", msg) as reply from q2 where uid=" + request.getParameter("userid") + " and ts='" + request.getParameter("tweet_time") + "' order by tid;";
+		out.println(query);
 		Statement st = conn.createStatement();
 		ResultSet rs = st.executeQuery(query);
 		while (rs.next())
