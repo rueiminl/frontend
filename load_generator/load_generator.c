8eeb8b3cb833ee3150b3b9d162e839ddf6a8bcd2
Ray0617 <lrm0617@gmail.com>
2014-11-05 15:34:30 -0500

    load generator

--- load_generator/load_generator.c
+++ load_generator/load_generator.c
@@ -1,36 +1,66 @@
 #include <curl/curl.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define MAX_QUESTION 4
+
+void usage(char* arg0)
+{
+	printf("usage: %s server_location number\n", arg0);
+	printf("\teg. %s pretest-1993534053.us-east-1.elb.amazonaws.com 100\n", arg0);
+}
+
 int main(int argc, char* argv[])
 {
 	CURL *curl;
 	CURLcode res;
 	int i;
+	int r;
 	int ret;
 	int len;
-	FILE* file;
+	int num;
+	FILE* file[MAX_QUESTION];
 	char url[1024];
+	int count[MAX_QUESTION] = {0};
 
-	file = fopen("q1.txt", "r");
-	if (file)
+	if (argc < 3)
 	{
-		curl = curl_easy_init();
-		if (curl)
-		{	
-			sprintf(url, "http://localhost:8080");
+		usage(argv[0]);
+		return 0;
+	}
+	num = atoi(argv[2]);
+	if (num <= 0)
+	{
+		usage(argv[0]);
+		return 0;
+	}
+
+	for (i = 0; i < MAX_QUESTION; i++)
+	{
+		sprintf(url, "q%d.txt", i+1);
+		file[i] = fopen(url, "r");
+	}
+	curl = curl_easy_init();
+	if (curl)
+	{	
+		for (i = 0; i < num; i++)
+		{
+			sprintf(url, "http://%s", argv[1]);
 			len = strlen(url);
-			fgets(url + len, sizeof(url) - len, file);
+			r = rand() % MAX_QUESTION;
+			fgets(url + len, sizeof(url) - len, file[r]);
 			curl_easy_setopt(curl, CURLOPT_URL, url);
-			//printf("url=%s\n", url);
-			for (i = 0; i < 1000; i++)
-			{
-				res = curl_easy_perform(curl);
-				if (res != CURLE_OK)
-					fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-			}
-			curl_easy_cleanup(curl);
+			res = curl_easy_perform(curl);
+			if (res != CURLE_OK)
+				fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 		}
-		fclose(file);
+		curl_easy_cleanup(curl);
+	}
+	for (i = 0; i < MAX_QUESTION; i++)
+	{
+		if (file[i])
+			fclose(file[i]);
 	}
 	return 0;
 }
